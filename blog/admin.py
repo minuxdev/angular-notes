36161ef27bb67226bc3ab5a431f04674d4b9c830
minuxdev <minux.midi@gmail.com>
2023-12-13 00:52:44 +0200

    registered models in admin panel.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "total_post")
+    list_filter = ("name",)
+    ordering = (
+        "-id",
+        "name",
+    )
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("author", "category", "topic", "views", "updated_on")
+    list_filter = ("author", "category", "topic")
+    ordering = ("-updated_on", "-id")
